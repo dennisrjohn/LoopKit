046ce7ea2a6ccc7a689a772b779107462b68ff91
Dennis John <dennis.r.john@gmail.com>
2021-02-05 17:21:10 -0700

    Testing SPM

---
+++ Package.swift
@@ -0,0 +1,32 @@
+import PackageDescription
+
+let package = Package(
+    name: "Shared",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "LoopKit",
+            targets: ["Shared"]),
+        .library(
+            name: "LoopKitUI",
+            targets: ["Shared"]),
+        .library(
+            name: "MockKit",
+            targets: ["Shared"]),
+        .library(
+            name: "MockKitUI",
+            targets: ["Shared"]),
+        .library(
+            name: "LoopTestingKit",
+            targets: ["Shared"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Shared",
+            dependencies: [],
+            path: "LoopKit")
+    ]
+)
